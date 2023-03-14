f967f6d75889bb5c64f7ba91edf660ec032ad1da
SENACRJEDU\36124872021.1N <a@a.com>
2023-03-14 20:45:06 -0300

    feat: user service and controller

---
+++ src/main/java/br/com/senac/controller/EstudanteController.java
@@ -0,0 +1,31 @@
+package br.com.senac.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import br.com.senac.domain.Estudante;
+import br.com.senac.service.EstudanteService;
+
+@RestController()
+@RequestMapping("estudante")
+public class EstudanteController {
+	
+	@Autowired
+	private EstudanteService estudanteService;
+	
+	@GetMapping("/{id}")
+	public ResponseEntity<Estudante> getEstudantePorId(@PathVariable Long id) {
+		return estudanteService.buscaEstudantePorId(id);
+	}
+	
+	@GetMapping
+	public ResponseEntity<List<Estudante>> buscarTodosEstudantes(){
+		return estudanteService.buscarTodosEstudantes();
+	}
+}
